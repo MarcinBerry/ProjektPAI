659959ef00f5c98cab22c151501713ab838c6ed4
MarcinBerry <71261155+MarcinBerry@users.noreply.github.com>
2021-06-09 07:13:33 +0200

    Dodawanie kolorowania i animacji przycisków
    Próba 1

--- JS/home.js
+++ JS/home.js
@@ -1,21 +1,31 @@
-/*
-$(document).ready(function() {
-    $('#przyciski li').on('mouseenter', function() {
-        $('#przyciski li').animate({background: '#000000'}, 20, 'linear');
-    });
-});
-*/
 function homeInit() {
     $(document).ready(function () { 
         $('main').fadeIn(500).css("display", "flex");
+        var buttonsCount = $('.przyciski');
+        var testText = "";
+        var tab = [];
+        for(var i = 1; i <= buttonsCount.length; i++) {
+            tab[i] = i*50;
+            $('.przyciski:nth-child('+i+')').css("background", "rgb("+tab[i]+", 150, 150)");
+            $('.przyciski:nth-child(n)').css("transition", "background 0.5s");
+            
+        }
+
+        // $('#przycisk1').mouseenter(function() {
+        //     $(this).css("background", "transparent");
+        // });
+       
+
+        $('.przyciski:nth-child(n)').mouseenter(function() {
+            $(this).css("background", "transparent");
+        })
+
+        $('.przyciski:nth-child(n)').mouseleave(function() {
+            $(this).css("background", "#ffff00");
+        })
+
+        $('#test').text(testText)
+        
     });
 
-}
-function changeColor() {
-    $('#przycisk1').on('click', function() {
-        $('#effect').animate({
-            backgroundColor: "#000000"
-        }, 200);
-    });
-    //$('#przycisk1').css("background", "var(--tlo-kolor)");
 }
