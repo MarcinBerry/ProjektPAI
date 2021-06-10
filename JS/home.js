f189109afb608ce9f1eac8a054bc80bc3be6e13f
Marcin Bernecki <berneckimarcin.priv@gmail.com>
2021-06-10 13:36:56 +0200

    Próba wyodrębnienia animacji od generowania strony

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,5 @@
 function homeInit() {
+    generujHome();
     $(document).ready(function () { 
         $('main').fadeIn(500).css("display", "flex");
         konfigurujPrzyciski();
@@ -8,20 +9,21 @@ function homeInit() {
 function przejdzDo(strona) {
     switch(strona) 
     {
-        case 'projekty': generujProjekty(); break;
-        case 'home': generujHome(); break;
+        case 'projekty': shiftTo(generujProjekty()); break;
+        case 'home': shiftTo(generujHome()); break;
         default: alert("coś poszło nie tak");
     }
 }
 
 function generujProjekty() {
-    $('main').fadeOut(500, function() {
-        $('main').html("<h1>Projekty</h1>");
-        for(var i = 0; i < 3; i++) {
-            
-            generujProjekt();
-        }
-    }).fadeIn(500);
+    $('main').attr("id", "projekty").html("<h1>Projekty</h1>");
+    for(var i = 0; i < 3; i++) {            
+        generujProjekt();
+    }
+}
+
+function shiftTo(funkcja) {
+    $('main').fadeOut(500, funkcja()).fadeIn(500);
 }
 
 function generujProjekt(tytul, opis, zdjecie) {
@@ -29,52 +31,51 @@ function generujProjekt(tytul, opis, zdjecie) {
     var op = opis;
     var zd = zdjecie;
     
+    //Kontrola wejścia
     if(ty === undefined) {
         ty = "Przykładowy tytuł";
     }
-
     if(op === undefined) {
         op = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     }
     if(zd === undefined) {
         zd = "projekt.jpg";
     }
-    $('main').attr("id", "projekty").append(
-        '<article class="projekt">'
-        +'    <div class="opis-projektu">'
-        +'      <h3>'+ty+'</h3>'
-        +'      <p>'+op+'</p>'
-        +'    </div>'
-        +'   <div class="zdjecie-projektu">'
-        +'       <img src="img/'+zd+'"/>'
-        +'    </div>'
-        +'</article>'
-    ).css("flex-flow", "column")
+
+    var content = '<article class="projekt">'
+    +'    <div class="opis-projektu">'
+    +'      <h3>'+ty+'</h3>'
+    +'      <p>'+op+'</p>'
+    +'    </div>'
+    +'   <div class="zdjecie-projektu">'
+    +'       <img src="img/'+zd+'"/>'
+    +'    </div>'
+    +'</article>';
+
+    $('main').append(content).css("flex-flow", "column");
 }
 
 function generujHome() {
-    $('main').fadeOut(500, function() {
-        $('main').html(
-            '<section>'
-            +'<img id="profilowe" src="img/profilowe.png"/>'
-            +'</section>'
-            +'<section>'
-            + '<h1>Witaj</h1>'
-            + '<h3>Trochę o mnie</h3>'
-            + '<span id="test"></span>'
-            + '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-            + 'Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit. '
-            + 'Id ornare arcu odio ut sem nulla pharetra. </p>'
-            + '<div class="przyciski-box">'
-            + '<div class="przyciski" onclick="przejdzDo(\'wiecej\')">Więcej</div>'
-            + '<div class="przyciski" onclick="przejdzDo(\'projekty\')">Projekty</div>'
-            + '<div class="przyciski" onclick="przejdzDo(\'kontakt\')">Kontakt</div>'
-            + '</div>'
-            + '</div>'
-            +'</section>'
-        ).css("flex-flow", "row")
-        konfigurujPrzyciski();
-    }).fadeIn(500);
+    var content = '<section>'
+    +'<img id="profilowe" src="img/profilowe.png"/>'
+    +'</section>'
+    +'<section>'
+    + '<h1>Witaj</h1>'
+    + '<h3>Trochę o mnie</h3>'
+    + '<span id="test"></span>'
+    + '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    + 'Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit. '
+    + 'Id ornare arcu odio ut sem nulla pharetra. </p>'
+    + '<div class="przyciski-box">'
+    + '<div class="przyciski" onclick="przejdzDo(\'wiecej\')">Więcej</div>'
+    + '<div class="przyciski" onclick="przejdzDo(\'projekty\')">Projekty</div>'
+    + '<div class="przyciski" onclick="przejdzDo(\'kontakt\')">Kontakt</div>'
+    + '</div>'
+    + '</div>'
+    +'</section>';
+
+    $('main').attr("id", "home").html(content).css("flex-flow", "row")
+    konfigurujPrzyciski();
 }
 
 function konfigurujPrzyciski() {
