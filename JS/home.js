d710e5090aa0eb3540ce936af6480883bc5b8f9a
Marcin Bernecki <berneckimarcin.priv@gmail.com>
2021-06-10 11:46:59 +0200

    Optymalizacja, zautomatyzowanie dodawania projektów do strony

--- JS/home.js
+++ JS/home.js
@@ -1,45 +1,55 @@
 function homeInit() {
     $(document).ready(function () { 
         $('main').fadeIn(500).css("display", "flex");
-        kolorujPrzyciski();
+        konfigurujPrzyciski();
     });  
 }
-
-// function przejdzDo(strona) {
-//     $('main').fadeOut(500, function() {
-//         $('main').load(strona, function() {
-//             alert("load was performed");
-//         });
-//     }).fadeIn(500);
-    
-// }
 
 function przejdzDo(strona) {
     switch(strona) 
     {
-        case 'projekty': generujProjekt(); break;
+        case 'projekty': generujProjekty(); break;
         case 'home': generujHome(); break;
         default: alert("coś poszło nie tak");
     }
 }
 
-function generujProjekt() {
+function generujProjekty() {
     $('main').fadeOut(500, function() {
-        $('main').attr("id", "projekty").html(
-            '<h1>Projekty</h1>'
-            +'<article>'
-            +'    <div class="opis-projektu">'
-            +'      <h3>Tytul Projektu</h3>'
-            +'      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            +'      Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit.'
-            +'      Id ornare arcu odio ut sem nulla pharetra. </p>'
-            +'    </div>'
-            +'   <div class="zdjecie-projektu">'
-            +'       <img src="img/projekt.jpg"/>'
-            +'    </div>'
-            +'</article>'
-        ).css("flex-flow", "column")
+        $('main').html("<h1>Projekty</h1>");
+        for(var i = 0; i < 3; i++) {
+            
+            generujProjekt();
+        }
     }).fadeIn(500);
+}
+
+function generujProjekt(tytul, opis, zdjecie) {
+    var ty = tytul;
+    var op = opis;
+    var zd = zdjecie;
+    
+    if(ty === undefined) {
+        ty = "Przykładowy tytuł";
+    }
+
+    if(op === undefined) {
+        op = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    }
+    if(zd === undefined) {
+        zd = "projekt.jpg";
+    }
+    $('main').attr("id", "projekty").append(
+        '<article class="projekt">'
+        +'    <div class="opis-projektu">'
+        +'      <h3>'+ty+'</h3>'
+        +'      <p>'+op+'</p>'
+        +'    </div>'
+        +'   <div class="zdjecie-projektu">'
+        +'       <img src="img/'+zd+'"/>'
+        +'    </div>'
+        +'</article>'
+    ).css("flex-flow", "column")
 }
 
 function generujHome() {
@@ -63,11 +73,11 @@ function generujHome() {
             + '</div>'
             +'</section>'
         ).css("flex-flow", "row")
-        kolorujPrzyciski();
+        konfigurujPrzyciski();
     }).fadeIn(500);
 }
 
-function kolorujPrzyciski() {
+function konfigurujPrzyciski() {
     var buttonsCount = $('.przyciski'); 
     for(var i = 1; i <= buttonsCount.length; i++) {
         $('.przyciski:nth-child('+i+')').css("background", "var(--color"+(i+1)+")");
