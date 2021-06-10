d248dde1d412be082047211e227c473eb4e0984d
MarcinBerry <71261155+MarcinBerry@users.noreply.github.com>
2021-06-10 20:37:17 +0200

    Mechanizm kopiowania home do zmiennej

--- JS/home.js
+++ JS/home.js
@@ -1,8 +1,8 @@
-var homeContent = "12";
+var homeContent = "";
 
 function homeInit() {
     $(document).ready(function () { 
-        homeContent = $('main').clone(true);
+        homeContent = $('main').html();
         $('main').fadeIn(500).css("display", "flex");
         kolorujPrzyciski();
     });  
@@ -11,52 +11,58 @@ function homeInit() {
 function przejdzDo(strona) {
     switch(strona) 
     {
-        case 'projekty': generujProjekt(); break;
+        case 'projekty': generujProjekty(); break;
         case 'home': generujHome(); break;
+        case 'kontakt': generujKontakt(); break;
         default: alert("coś poszło nie tak");
     }
 }
 
-function generujProjekt() {
+function generujKontakt() {
+    
+}
+
+function generujProjekty() {
     $('main').fadeOut(500, function() {
-        $('main').attr("id", "projekty").html(
-            '<h1>Projekty</h1>'
-            +'<article>'
-            +'    <div class="opis-projektu">'
-            +'      <h3>Tytul Projektu</h3>'
-            +'      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            +'      Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit.'
-            +'      Id ornare arcu odio ut sem nulla pharetra. </p>'
-            +'    </div>'
-            +'   <div class="zdjecie-projektu">'
-            +'       <img src="img/projekt.jpg"/>'
-            +'    </div>'
-            +'</article>'
-        ).css("flex-flow", "column")
+        $('main').html("<h1>Projekty</h1>");
+        for(var i = 0; i < 3; i++) {
+            generujProjekt();
+        }
     }).fadeIn(500);
+}
+
+function generujProjekt(tytul, opis, zdjecie) {
+    var ty = tytul;
+    var op = opis;
+    var zd = zdjecie;
+    
+    if(ty === undefined) {
+        ty = "Przykładowy tytuł";
+    }
+
+    if(op === undefined) {
+        op = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    }
+    if(zd === undefined) {
+        zd = "projekt.jpg";
+    }
+
+    var content = '<article class="projekt">'
+    +'    <div class="opis-projektu">'
+    +'      <h3>'+ty+'</h3>'
+    +'      <p>'+op+'</p>'
+    +'    </div>'
+    +'   <div class="zdjecie-projektu">'
+    +'       <img src="img/'+zd+'"/>'
+    +'    </div>'
+    +'</article>';
+
+    $('main').attr("id", "projekty").append(content).css("flex-flow", "column")
 }
 
 function generujHome() {
     $('main').fadeOut(500, function() {
-        $('main').html(
-            '<section>'
-            +'<img id="profilowe" src="img/profilowe.png"/>'
-            +'</section>'
-            +'<section>'
-            + '<h1>Witaj</h1>'
-            + '<h3>Trochę o mnie</h3>'
-            + '<span id="test"></span>'
-            + '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-            + 'Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit. '
-            + 'Id ornare arcu odio ut sem nulla pharetra. </p>'
-            + '<div class="przyciski-box">'
-            + '<div class="przyciski" onclick="przejdzDo(\'wiecej\')">Więcej</div>'
-            + '<div class="przyciski" onclick="przejdzDo(\'projekty\')">Projekty</div>'
-            + '<div class="przyciski" onclick="przejdzDo(\'kontakt\')">Kontakt</div>'
-            + '</div>'
-            + '</div>'
-            +'</section>'
-        ).attr("id", "home").css("flex-flow", "row");
+        $('main').html(homeContent).attr("id", "home").css("flex-flow", "row");
         kolorujPrzyciski();
     }).fadeIn(500);
 }
