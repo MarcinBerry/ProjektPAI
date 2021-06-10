d6af2cde36491f5c934ebe1536f323cf8cf8b8ce
MarcinBerry <71261155+MarcinBerry@users.noreply.github.com>
2021-06-10 07:21:57 +0200

    Dodawanie strony projektów i przycisku "home"

--- JS/home.js
+++ JS/home.js
@@ -23,37 +23,61 @@ function homeInit() {
 
 }
 
-function przejdzDo(strona) {
-    $('main').fadeOut(500, function() {
-        $('main').load(strona, function() {
-            alert("load was performed");
-        });
-    }).fadeIn(500);
+// function przejdzDo(strona) {
+//     $('main').fadeOut(500, function() {
+//         $('main').load(strona, function() {
+//             alert("load was performed");
+//         });
+//     }).fadeIn(500);
     
-}
+// }
 
-function zmienDo(strona) {
+function przejdzDo(strona) {
     switch(strona) 
     {
         case 'projekty': generujProjekty(); break;
+        case 'home': generujHome(); break;
         default: alert("coś poszło nie tak");
     }
 }
 
 function generujProjekty() {
-    $('main').html(
-        '<h1>Projekty</h1>'
-        +'<article>'
-        +'    <div class="opis-projektu">'
-        +'      <h3>Tytul Projektu</h3>'
-        +'      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        +'      Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit.'
-        +'      Id ornare arcu odio ut sem nulla pharetra. </p>'
-        +'    </div>'
-        +'   <div class="zdjecie-projektu">'
-        +'       <img src="img/projekt.jpg"/>'
-        +'    </div>'
-        +'</article>'
-    ).css("flex-flow", "column");
-    $('article').css("display", "flex", "flex-flow", "row", "text-align", "left", "justify-content", "space-between", "width", "75%");
+    $('main').fadeOut(500, function() {
+        $('main').html(
+            '<h1>Projekty</h1>'
+            +'<article>'
+            +'    <div class="opis-projektu">'
+            +'      <h3>Tytul Projektu</h3>'
+            +'      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            +'      Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit.'
+            +'      Id ornare arcu odio ut sem nulla pharetra. </p>'
+            +'    </div>'
+            +'   <div class="zdjecie-projektu">'
+            +'       <img src="img/projekt.jpg"/>'
+            +'    </div>'
+            +'</article>'
+        ).css("flex-flow", "column")
+    }).fadeIn(500);
+}
+function generujHome() {
+    $('main').fadeOut(500, function() {
+        $('main').html(
+            +'<section>'
+            +'<img id="profilowe" src="img/profilowe.png"/>'
+            +'</section>'
+            +'<section>'
+            + '<h1>Witaj</h1>'
+            + '<h3>Trochę o mnie</h3>'
+            + '<span id="test"></span>'
+            + '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+            + 'Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit. '
+            + 'Id ornare arcu odio ut sem nulla pharetra. </p>'
+            + '<div class="przyciski-box">'
+            + '<div class="przyciski" onclick="przejdzDo()">Kontynuuj</div>'
+            + '<div class="przyciski" >Projekty</div>'
+            + '<div class="przyciski" >Kontakt</div>'
+            + '</div>'
+            +'</section>'
+        ).css("flex-flow", "row")
+    }).fadeIn(500);
 }
