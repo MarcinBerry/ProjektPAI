b161da14dffb0814b2e21216d235599625220ec5
Marcin Bernecki <berneckimarcin.priv@gmail.com>
2021-06-10 14:03:00 +0200

    Przywrócenie przed wyodrębnianiem animacji

--- JS/home.js
+++ JS/home.js
@@ -2,7 +2,6 @@ function homeInit() {
     generujHome();
     $(document).ready(function () { 
         $('main').fadeIn(500).css("display", "flex");
-        konfigurujPrzyciski();
     });  
 }
 
@@ -11,6 +10,9 @@ function przejdzDo(strona) {
     {
         case 'projekty': shiftTo(generujProjekty()); break;
         case 'home': shiftTo(generujHome()); break;
+=======
+        case 'projekty': generujProjekt(); break;
+        case 'home': generujHome(); break;
         default: alert("coś poszło nie tak");
     }
 }
@@ -76,9 +78,51 @@ function generujHome() {
 
     $('main').attr("id", "home").html(content).css("flex-flow", "row")
     konfigurujPrzyciski();
+=======
+function generujProjekt() {
+    $('main').fadeOut(500, function() {
+        $('main').attr("id", "projekty").html(
+            '<h1>Projekty</h1>'
+            +'<article>'
+            +'    <div class="opis-projektu">'
+            +'      <h3>Tytul Projektu</h3>'
+            +'      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            +'      Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit.'
+            +'      Id ornare arcu odio ut sem nulla pharetra. </p>'
+            +'    </div>'
+            +'   <div class="zdjecie-projektu">'
+            +'       <img src="img/projekt.jpg"/>'
+            +'    </div>'
+            +'</article>'
+        ).css("flex-flow", "column")
+    }).fadeIn(500);
 }
 
-function konfigurujPrzyciski() {
+function generujHome() {
+    $('main').fadeOut(500, function() {
+        $('main').html(
+            '<section>'
+            +'<img id="profilowe" src="img/profilowe.png"/>'
+            +'</section>'
+            +'<section>'
+            + '<h1>Witaj</h1>'
+            + '<h3>Trochę o mnie</h3>'
+            + '<span id="test"></span>'
+            + '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+            + 'Sed cras ornare arcu dui vivamus arcu felis bibendum. At lectus urna duis convallis convallis tellus id interdum velit. '
+            + 'Id ornare arcu odio ut sem nulla pharetra. </p>'
+            + '<div class="przyciski-box">'
+            + '<div class="przyciski" onclick="przejdzDo(\'wiecej\')">Więcej</div>'
+            + '<div class="przyciski" onclick="przejdzDo(\'projekty\')">Projekty</div>'
+            + '<div class="przyciski" onclick="przejdzDo(\'kontakt\')">Kontakt</div>'
+            + '</div>'
+            + '</div>'
+            +'</section>'
+        ).css("flex-flow", "row")
+        kolorujPrzyciski();
+    }).fadeIn(500);
+}
+
     var buttonsCount = $('.przyciski'); 
     for(var i = 1; i <= buttonsCount.length; i++) {
         $('.przyciski:nth-child('+i+')').css("background", "var(--color"+(i+1)+")");
