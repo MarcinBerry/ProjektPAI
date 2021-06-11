4a505bfc615182309263cfcb1ea0d37b4ee0b54d
Marcin Bernecki <berneckimarcin.priv@gmail.com>
2021-06-11 10:01:00 +0200

    Wyodrębnienie funkcji zajmujących się generowanie innymi stronami do oddzielnych plików js

--- JS/home.js
+++ JS/home.js
@@ -12,7 +12,6 @@ function homeInit() {
 
 function testowaFunkcja() {
 
-
 }
 
 function generujPrzycisk(divBox, odnosnik, tytul) {
@@ -35,53 +34,15 @@ function generujPrzycisk(divBox, odnosnik, tytul) {
 function przejdzDo(strona) {
     switch(strona) 
     {
+        case 'wiecej': generujWiecej(); break;
         case 'projekty': generujProjekty(); break;
         case 'home': generujHome(); break;
         default: alert("coś poszło nie tak");
     }
 }
 
-function generujProjekty() {
-    $('main').fadeOut(500, function() {
-        $('main').html("<h1>Projekty</h1>");
-        for(var i = 0; i < 3; i++) {
-            generujProjekt();
-        }
-    }).fadeIn(500);
-}
-
-function generujProjekt(tytul, opis, zdjecie) {
-    var ty = tytul;
-    var op = opis;
-    var zd = zdjecie;
-    
-    if(ty === undefined) {
-        ty = "Przykładowy tytuł";
-    }
-
-    if(op === undefined) {
-        op = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    }
-    if(zd === undefined) {
-        zd = "projekt.jpg";
-    }
-
-    var content = '<article class="projekt">'
-    +'    <div class="opis-projektu">'
-    +'      <h3>'+ty+'</h3>'
-    +'      <p>'+op+'</p>'
-    +'    </div>'
-    +'   <div class="zdjecie-projektu">'
-    +'       <img src="img/'+zd+'"/>'
-    +'    </div>'
-    +'</article>';
-
-    $('main').attr("id", "projekty").append(content).css("flex-flow", "column")
-}
-
 function generujHome() {
     $('main').fadeOut(500, function() {
         $('main').html(homeContent).attr("id", "home").css("flex-flow", "row");
-        kolorujPrzyciski();
     }).fadeIn(500);
 }
---
+++ JS/projekty.js
@@ -0,0 +1,37 @@
+function generujProjekty() {
+    $('main').fadeOut(500, function() {
+        $('main').html("<h1>Projekty</h1>");
+        for(var i = 0; i < 3; i++) {
+            generujProjekt();
+        }
+    }).fadeIn(500);
+}
+
+function generujProjekt(tytul, opis, zdjecie) {
+    var ty = tytul;
+    var op = opis;
+    var zd = zdjecie;
+    
+    if(ty === undefined) {
+        ty = "Przykładowy tytuł";
+    }
+
+    if(op === undefined) {
+        op = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    }
+    if(zd === undefined) {
+        zd = "projekt.jpg";
+    }
+
+    var content = '<article class="projekt">'
+    +'    <div class="opis-projektu">'
+    +'      <h3>'+ty+'</h3>'
+    +'      <p>'+op+'</p>'
+    +'    </div>'
+    +'   <div class="zdjecie-projektu">'
+    +'       <img src="img/'+zd+'"/>'
+    +'    </div>'
+    +'</article>';
+
+    $('main').attr("id", "projekty").append(content).css("flex-flow", "column")
+}
