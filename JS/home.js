aede4c4abcec7191e5565865f648153700c8f93c
Marcin Bernecki <berneckimarcin.priv@gmail.com>
2021-06-11 14:43:44 +0200

    Dodawania strony "więcej"

--- JS/home.js
+++ JS/home.js
@@ -2,10 +2,10 @@ var homeContent = "";
 
 function homeInit() {
     $(document).ready(function () { 
-        generujPrzycisk(".przyciski-box", "wiecej", "Wiecej");
+        homeContent = $('main').html();
+        generujPrzycisk(".przyciski-box", "wiecej", "Więcej");
         generujPrzycisk(".przyciski-box", "projekty", "Projekty");
         generujPrzycisk(".przyciski-box", "kontakt", "Kontakt");
-        homeContent = $('main').html();
         $('main').fadeIn(500).css("display", "flex");
     });  
 }
@@ -44,5 +44,44 @@ function przejdzDo(strona) {
 function generujHome() {
     $('main').fadeOut(500, function() {
         $('main').html(homeContent).attr("id", "home").css("flex-flow", "row");
+        generujPrzycisk(".przyciski-box", "wiecej", "Wiecej");
+        generujPrzycisk(".przyciski-box", "projekty", "Projekty");
+        generujPrzycisk(".przyciski-box", "kontakt", "Kontakt");
     }).fadeIn(500);
+}
+
+function generujWiecej() {
+    var content = "<h3>Więcej</h3>"
+    +"<section class='doswiadczenie'>"
+    +"<h4>Doświadczenie</h4>"
+    +"</section>"
+
+    $('main').fadeOut(500, function() {
+        $('main').html(content).attr("id", "projekty").css("flex-flow", "column");
+        generujWpis();
+    }).fadeIn(500);
+}
+
+function generujWpis(data, tytul, opis) {
+    var ty = tytul;
+    var op = opis;
+    var dat = data;
+    
+    if(ty === undefined) {
+        ty = "Przykładowy tytuł";
+    }
+
+    if(op === undefined) {
+        op = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    }
+    if(dat === undefined) {
+        dat = "2020 - Present";
+    }
+    
+    var content = 
+    "<div><p>"+data+"</p></div"
+    +"<div><h5>"+tytul+"</h5>"
+    +"<p>"+opis+"</p><d/iv>"
+
+    $('.doswiadczenie').append(content);
 }
