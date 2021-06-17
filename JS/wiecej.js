8851780c0ae6e43fa4d0f0767a8ff311679c69d7
Marcin Bernecki <berneckimarcin.priv@gmail.com>
2021-06-17 13:59:19 +0200

    Ułożenie strony "więcej" w css

--- JS/wiecej.js
+++ JS/wiecej.js
@@ -7,25 +7,25 @@ function generujWiecej() {
     }).fadeIn(500);
 }
 
-function generujRozdzial(klasa = "przykladowa", tytul = "Przykładowy tytuł") {
+function generujRozdzial(id = "przykladowa", tytul = "Przykładowy tytuł") {
     var content = 
-    "<section class='chapter' id='"+klasa+"'>"
-    +"<div class='"+klasa+"-title'><h4>"+tytul+"</h4></div>"
-    +"<div class='admissions' id='"+klasa+"-admissions'></div></section>"
-    +"<hr/>";
+    "<section class='chapter' id='"+id+"'>"
+    +"<div class='"+id+"-title'><h4 class='admissions-title'>"+tytul+"</h4></div>"
+    +"<div class='admissions' id='"+id+"-admissions'></div></section>"
+    +"<hr style='width: 100%;'/>";
 
     $('main').append(content);
 }
 
-function generujWpis(klasa = "przykladowa",
+function generujWpis(id = "przykladowa",
     data = "2020 - TEST",
     tytul = "Przykładowy tytuł",
     opis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.") 
     {
     var content = 
-    "<div class='entry'><div><p>"+data+"</p></div>"
-    +"<div><h5>"+tytul+"</h5>"
-    +"<p>"+opis+"</p></div></div>"
+    "<div class='entry'><div><p class='entry-date'>"+data+"</p></div>"
+    +"<div class='entry-desc'><h5 class='entry-title'>"+tytul+"</h5>"
+    +"<p class='entry-text'>"+opis+"</p></div></div>"
 
-    $(klasa).append(content);
+    $(id).append(content);
 }
